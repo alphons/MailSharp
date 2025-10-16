0caa41c613b4ba59456fd170df0c6f06d5530777
alphons <alphons@heijden.com>
2025-10-16 13:19:21 +0200

    and a todo

--- MailSharp.Smtp/Session/DATAheaderend.cs
+++ MailSharp.Smtp/Session/DATAheaderend.cs
@@ -11,6 +11,11 @@ public partial class SmtpSession
 			await writer.WriteLineAsync(configuration["SmtpResponses:BadSequence"], ct);
 			return;
 		}
+
+		// TODO check if header is valid (e.g. has From, To, Date fields)
+		// If not, respond with 554 and reset state to RcptToReceived
+		// Also check for known spam such as Subject: Buy now, etc.
+
 		state = SmtpState.DataStarted;
 	}
 
